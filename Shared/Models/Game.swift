924545df8f9a0379794916995ace5326d70bcf54
Hyeonsang <250wons@gmail.com>
2022-02-15 13:41:33 +0900

    add: Alert Message with Button Press, Slider Movement, Slider Value Detecting, App Organization, Model Creation

---
+++ Shared/Models/Game.swift
@@ -0,0 +1,19 @@
+//
+//  Game.swift
+//  Bullseye (iOS)
+//
+//  Created by Noah's Ark on 2022/02/15.
+//
+
+import Foundation
+
+struct Game {
+    
+    var target: Int = 42
+    var score: Int = 0
+    var round: Int = 1
+    
+    func points(sliderValue: Int) -> Int {
+        return 999
+    }
+}
--- Shared/Views/ContentView.swift
+++ Shared/Views/ContentView.swift
@@ -6,7 +6,11 @@ import SwiftUI
 
 // Instances with struct
 struct ContentView: View {
-    // Data
+
+    @State private var alertIsVisible: Bool = false
+    @State private var sliderValue: Double = 50.0
+    @State private var game: Game = Game()
+
     var body: some View {
         VStack {
             Text("🇰🇷\nPut the Bullseye as close as you can to")
@@ -16,20 +20,27 @@ struct ContentView: View {
                 .multilineTextAlignment(.center)
                 .lineSpacing(4.0)
                 .font(.footnote)
-            Text("89")
+            Text(String(game.target))
                 .font(.largeTitle)
                 .fontWeight(.black)
                 .kerning(0)
             HStack {
                 Text("1")
                     .bold()
-                Slider(value: .constant(50), in: 1.0...100.0)
+                Slider(value: self.$sliderValue, in: 1.0...100.0)
                 Text("100")
                     .bold()
             }
-            Button(action: {}) {
+            Button(action: {
+                self.alertIsVisible = true
+            }) {
                 Text("HIT ME")
             }
+            .alert(isPresented: $alertIsVisible,
+                content: {
+                var roundedValue: Int = Int(self.sliderValue.rounded())
+                return Alert(title: Text("Hello There"), message: Text("The slider's value is \(roundedValue).\n" + "You scored \(self.game.points(sliderValue: roundedValue)) points this round."), dismissButton: .default(Text("Awesome")))
+            })
         }
     }
 }
